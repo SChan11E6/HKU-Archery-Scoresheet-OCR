a26ac68d939e8187ef1cdd00ffd925699fd073c5
Samson Chan <94214942+SChan11E6@users.noreply.github.com>
2024-11-28 23:53:09 +0800

    Add files via upload

---
+++ run_ocr_windows.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+set VENV_PATH=archery_ocr
+set PYTHON_EXEC=%VENV_PATH%\Scripts\pythonw.exe
+
+REM Check if the Python executable exists
+if exist "%PYTHON_EXEC%" (
+    start "" "%PYTHON_EXEC%" ocr.cpython-312.pyc
+)
+exit
